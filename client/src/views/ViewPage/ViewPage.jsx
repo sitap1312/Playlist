7ec61942a8be64b8538c23e9492d7c07627f2c6d
Shahroze Husain <shahroze93@gmail.com>
2021-08-03 17:54:04 -0400

    figured out inital queue function for diff media

--- client/src/views/ViewPage/ViewPage.jsx
+++ client/src/views/ViewPage/ViewPage.jsx
@@ -1,23 +1,37 @@
 import React from 'react'
 import Layout from "../../components/Layout/Layout";
 import ReactPlayer from 'react-player'
+import { useState } from "react";
 
 export default function ViewPage() {
-      return (
+  // create variable that carries array of links for a playlist
+  const data = {}
+  // feed variable at index 0 into useState for currentVideo
+  // at the end of the video/music it will trigger playNext which will assign the setCurrentVideo to the next media in the playlist
+
+    const [currentVideo, setCurrentVideo] = useState("https://soundcloud.com/fearloathing/lofi-house-mix-1988-the-stoner-house-edition");
+    const playNext = () => {
+      // videoData.shift()
+      // if (videoData.length > 0) {
+      //   updateCurrentVideoId(videoData[0].id.videoId)
+      // }
+      setCurrentVideo("https://www.youtube.com/watch?v=BHACKCNDMW8")
+    }
+
+    return (
         <Layout>
           <h1></h1>
-          <ReactPlayer
+        <ReactPlayer
             className='react-player'
-            url='https://www.youtube.com/watch?v=BHACKCNDMW8'
-            // url='https://soundcloud.com/fearloathing/lofi-house-mix-1988-the-stoner-house-edition'
+            controls={true}
+            url = {currentVideo}
+            onEnded={playNext}
             width='100%'
             height='50vh'
           />
           <div>
             Playlist
-            
           </div>
         </Layout>
   )
 }
-
