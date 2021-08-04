6e9638128b6af85376dfdfd0b8f4454c31173931
Shahroze Husain <shahroze93@gmail.com>
2021-08-03 23:01:42 -0400

    Added playlist items, fixed bugs, added comments section

--- client/src/views/HomePage/HomePage.jsx
+++ client/src/views/HomePage/HomePage.jsx
@@ -1,11 +1,12 @@
-import { useState, useEffect } from 'react'
 import Layout from "../../components/Layout/Layout"
+import { Link } from "react-router-dom"
 
 export default function HomePage() {
 
   return (
     <Layout >
       <h1>PLAYLISTIFY</h1>
+      <Link to="/preview"><button>PREVIEW</button></Link>
     </Layout>
   );
 }
--- client/src/views/ViewPage/ViewPage.jsx
+++ client/src/views/ViewPage/ViewPage.jsx
@@ -1,76 +1,41 @@
 import React from 'react'
 import Layout from "../../components/Layout/Layout";
 import ReactPlayer from 'react-player'
-import { useState, useEffect } from "react";
+import { useState } from "react"
+import AllComments from '../AllComments/AllComments';
 
 export default function ViewPage() {
-  // create variable that carries array of links for a playlist
-  // feed variable at index 0 into useState for currentVideo
-  // at the end of the video/music it will trigger playNext which will assign the setCurrentVideo to the next media in the playlist
 
-    // const [currentVideo, setCurrentVideo] = useState("https://soundcloud.com/fearloathing/lofi-house-mix-1988-the-stoner-house-edition");
-    // const playNext = () => {
-    //   // videoData.shift()
-    //   // if (videoData.length > 0) {
-    //   //   updateCurrentVideoId(videoData[0].id.videoId)
-    //   // }
-    //   setCurrentVideo("https://www.youtube.com/watch?v=BHACKCNDMW8")
-    // }
-  
   // this is working with the data array
-  let data = [
-    "https://soundcloud.com/thundercat-official/dragonball-durag",
-    "https://soundcloud.com/thundercat-official/funny-thing",
-    "https://www.youtube.com/watch?v=jBuKNkVFaMU",
-    "https://soundcloud.com/brainfeeder/thundercat-them-changes"
-  ]
-    // const [currentVideo, setCurrentVideo] = useState(data[0]);
-    // const playNext = () => {
-    //   data.shift()
-    //   if (data.length > 0) {
-    //   setCurrentVideo(data[0])
-    //   }
-    // }
-
-    const [toggle, setToggle] = useState(false)
+  const data =
+    [
+      "https://www.youtube.com/watch?v=vVi4sHak14Q",
+      "https://www.youtube.com/watch?v=7hEPj13PUGc",
+      "https://www.youtube.com/watch?v=F2QyoxdGvbc",
+      "https://www.youtube.com/watch?v=oDlVYsghGA0"
+    ];
   
-    useEffect(() => {
-      playNext()
-    }, [toggle])
-  
-  let index = 0
-  const [currentVideo, setCurrentVideo] = useState(data[index]);
-  const playNext = () => {
-    if (data.length > 0) {
-      index++;
-      console.log(index)
-      setCurrentVideo(data[index])
-    }
-  }
-
-  const playPrevious = () => {
-    if (data.length > 0) {
-      index++;
-      console.log(index)
-      setCurrentVideo(data[index])
-    }
-  }
+    const playlistItems = data.map((link, index) =>
+    <li key={index}>
+      {link}
+    </li>
+  );
     
     return (
         <Layout>
           <h1></h1>
         <ReactPlayer
-            className='react-player'
-            controls={true}
-            url={currentVideo}
-            onEnded={playNext}
-            width='100%'
-            height='50vh'
+          className='react-player'
+          controls={true}
+          playing url={data}
+          loop={true}
+          width='100%'
+          height='50vh'
           />
           <div>
-          Playlist
-          <button onClick={playNext}>Next</button>
-          <button onClick={playPrevious}>Previous</button>
+          <h3>Playlist Items</h3>
+          <ul>{playlistItems}</ul>
+          <AllComments />
           </div>
         </Layout>
   )
