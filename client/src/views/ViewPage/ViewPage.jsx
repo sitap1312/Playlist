b29a0ae463653c79ce176445fc8ce62da1c6ec13
Shahroze Husain <shahroze93@gmail.com>
2021-08-04 01:35:09 -0400

    solved the next and prev track function with diff media

--- client/src/views/ViewPage/ViewPage.jsx
+++ client/src/views/ViewPage/ViewPage.jsx
@@ -7,34 +7,79 @@ import AllComments from '../AllComments/AllComments';
 export default function ViewPage() {
 
   // this is working with the data array
-  const data =
-    [
-      "https://www.youtube.com/watch?v=vVi4sHak14Q",
-      "https://www.youtube.com/watch?v=7hEPj13PUGc",
-      "https://www.youtube.com/watch?v=F2QyoxdGvbc",
-      "https://www.youtube.com/watch?v=oDlVYsghGA0"
-    ];
+  // const data =
+  //   [
+  //     "https://www.youtube.com/watch?v=vVi4sHak14Q",
+  //     "https://www.youtube.com/watch?v=7hEPj13PUGc",
+  //     "https://www.youtube.com/watch?v=F2QyoxdGvbc",
+  //     "https://www.youtube.com/watch?v=oDlVYsghGA0"
+  //   ];
   
-    const playlistItems = data.map((link, index) =>
-    <li key={index}>
-      {link}
-    </li>
+  const data = [
+    "https://soundcloud.com/thekidlaroi/stay",
+    "https://soundcloud.com/thekidlaroi/not-sober-feat-polo-g-stunna",
+    "https://www.youtube.com/watch?v=RVPeTSUzZ9I",
+    "https://soundcloud.com/drokenji/drokenji-since-december-prod-census-cv?in=soundcloud-hustle/sets/the-lookout-tomorrows-rap-hits",
+    "https://www.youtube.com/watch?v=Zj35vRgeXLs&t=1s",
+  ]
+
+  const playlistItems = data.map((link, index) =>
+  <div key={index}> 
+      <h3>{index}. {link}</h3>
+      {/* <ReactPlayer
+      className='react-player'
+      controls={true}
+      playing url = {link}
+      loop={true}
+      width='100%'
+      height='50vh'
+    /> */}
+  </div>
   );
-    
+
+  const [currentVideo, setCurrentVideo] = useState(data[0])
+  const [trackIndex, setTrackIndex] = useState(0);
+
+  const toPrevTrack = () => {
+    if (trackIndex - 1 < 0) {
+      setTrackIndex(data.length - 1);
+    } else {
+      setTrackIndex(trackIndex - 1);
+    }
+    fetchVideo()
+  }
+  
+  const toNextTrack = () => {
+    if (trackIndex < data.length - 1) {
+      setTrackIndex(trackIndex + 1);
+    } else {
+      setTrackIndex(0);
+    }
+    fetchVideo()
+  }
+
+  const fetchVideo = () => {
+    setCurrentVideo(data[trackIndex])
+  }
+
     return (
         <Layout>
-          <h1></h1>
-        <ReactPlayer
-          className='react-player'
-          controls={true}
-          playing url={data}
-          loop={true}
-          width='100%'
-          height='50vh'
-          />
+          <h1>PLAYLIST NAME</h1>
+          {/* <img src="https://wallpaperaccess.com/full/39608.jpg" width='500px' height="500px" /> */}
+        <h3>USERNAME</h3>
+            <ReactPlayer
+              className='react-player'
+              controls={true}
+              playing url = {currentVideo}
+              loop={true}
+              width='100%'
+              height='50vh'
+        />
+        <button onClick={toPrevTrack}>PREV</button>
+        <button onClick={toNextTrack}>NEXT</button>
           <div>
           <h3>Playlist Items</h3>
-          <ul>{playlistItems}</ul>
+          <div>{playlistItems}</div>
           <AllComments />
           </div>
         </Layout>
@@ -42,3 +87,4 @@ export default function ViewPage() {
 }
 
 // for the yellow warning for dev tools: https://stackoverflow.com/questions/61339968/devtools-failed-to-load-sourcemap-could-not-load-content-for-chrome-extension
+// REACTPLAYER can take in an array of youtube videos but cannot take in an array of soundcloud songs
