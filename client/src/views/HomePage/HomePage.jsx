48a42cf0a79cd4c3bb345d34f6af50bf96433836
Elijah Cruz <elijah@Elijahs-MacBook-Pro.local>
2021-08-04 07:59:30 -0700

    Getting updated viewpage

--- client/src/views/HomePage/HomePage.jsx
+++ client/src/views/HomePage/HomePage.jsx
@@ -1,11 +1,98 @@
 import { useState, useEffect } from 'react'
 import Layout from "../../components/Layout/Layout"
 
+let data = [
+  {
+    imgURL: "https://ninjatune.net/images/artists/thundercat-main.jpg",
+    name: "THUNDERCAT",
+    category: "Music",
+  },
+  {
+    imgURL: "https://pyxis.nymag.com/v1/imgs/3ae/a5b/e1a1c69441d44c72a86e1120d71f297423-04-mac-miller-2.rvertical.w570.jpg",
+    name: "MAC MILLER",
+    category: "Gaming"
+  },
+  { imgURL: "https://s1.ticketm.net/dam/a/384/2dbd2c80-4042-4429-b5fe-563f7227b384_1477701_TABLET_LANDSCAPE_LARGE_16_9.jpg", name: "JOHN MAYER",
+  category: "Sports" }
+]  
+
 export default function HomePage() {
+  const [playlist, setPlaylist] = useState(data)
+  
+  
+  const filterByCategory = (category) => {
+    const filtered = playlist.filter((cat) => cat.category === category)
+    return filtered
+  }
+  
 
   return (
     <Layout >
       <h1>PLAYLISTIFY</h1>
+      <h2>Discover</h2>
+      <div className="discoverPlaylist">
+      {playlist.map((playlist) => {
+        return (
+          <img src={playlist?.imgURL} alt={playlist.name} />
+        )
+      })}
+      </div>
+      <h2>Music</h2>
+      <div className="musicPlaylist">
+      {filterByCategory("Music").map((playlist) => {
+        return (
+          <img src={playlist?.imgURL} alt={playlist.name} />
+        )
+      })}
+      </div>
+      <h2>Videos</h2>
+      <div className="videoPlaylist">
+      {filterByCategory("Music").map((playlist) => {
+        return (
+          <img src={playlist?.imgURL} alt={playlist.name} />
+        )
+      })}
+      </div>
+      <h2>Gaming</h2>
+      <div className="videoPlaylist">
+      {filterByCategory("Gaming").map((playlist) => {
+        return (
+          <img src={playlist?.imgURL} alt={playlist.name} />
+        )
+      })}
+      </div>
+      <h2>Education</h2>
+      <div className="videoPlaylist">
+      {filterByCategory("Music").map((playlist) => {
+        return (
+          <img src={playlist?.imgURL} alt={playlist.name} />
+        )
+      })}
+      </div>
+      <h2>Sports</h2>
+      <div className="videoPlaylist">
+      {playlist.map((playlist) => {
+        return (
+          <img src={playlist?.imgURL} alt={playlist.name} />
+        )
+      })}
+      </div>
+      <h2>Entertainment</h2>
+      <div className="videoPlaylist">
+      {playlist.map((playlist) => {
+        return (
+          <img src={playlist?.imgURL} alt={playlist.name} />
+        )
+      })}
+      </div>
+      <h2>Family</h2>
+      <div className="videoPlaylist">
+      {playlist.map((playlist) => {
+        return (
+          <img src={playlist?.imgURL} alt={playlist.name} />
+        )
+      })}
+      </div>
     </Layout>
   );
 }
--- client/src/views/ViewPage/ViewPage.jsx
+++ client/src/views/ViewPage/ViewPage.jsx
@@ -1,23 +1,59 @@
 import React from 'react'
 import Layout from "../../components/Layout/Layout";
 import ReactPlayer from 'react-player'
-
+import { useState } from "react";
 export default function ViewPage() {
-      return (
+  // create variable that carries array of links for a playlist
+  // feed variable at index 0 into useState for currentVideo
+  // at the end of the video/music it will trigger playNext which will assign the setCurrentVideo to the next media in the playlist
+    // const [currentVideo, setCurrentVideo] = useState("https://soundcloud.com/fearloathing/lofi-house-mix-1988-the-stoner-house-edition");
+    // const playNext = () => {
+    //   // videoData.shift()
+    //   // if (videoData.length > 0) {
+    //   //   updateCurrentVideoId(videoData[0].id.videoId)
+    //   // }
+    //   setCurrentVideo("https://www.youtube.com/watch?v=BHACKCNDMW8")
+    // }
+  // this is working with the data array
+  let data = [
+    {url: "https://soundcloud.com/thundercat-official/dragonball-durag"},
+    {url: "https://soundcloud.com/thundercat-official/funny-thing"},
+    {url: "https://www.youtube.com/watch?v=jBuKNkVFaMU"},
+    {url: "https://soundcloud.com/brainfeeder/thundercat-them-changes"}
+  ]
+    const [videoIndex, setVideoIndex] = useState(0)
+    const [currentVideo, setCurrentVideo] = useState("");
+    const playNext = () => {
+      const nextIndex = videoIndex + 1
+      if (nextIndex >= data.length) {
+        currentVideo(data[0])
+        setVideoIndex(0)
+      } else {
+        setCurrentVideo(data[nextIndex])
+        setVideoIndex(nextIndex)
+      }
+    }
+  
+    const handleClick = (item, index) => {
+      setCurrentVideo(item.videoname)
+      setVideoIndex(index)
+    }
+    return (
         <Layout>
           <h1></h1>
-          <ReactPlayer
+        <ReactPlayer
             className='react-player'
-            url='https://www.youtube.com/watch?v=BHACKCNDMW8'
-            // url='https://soundcloud.com/fearloathing/lofi-house-mix-1988-the-stoner-house-edition'
+            controls={true}
+            url={currentVideo}
+            onEnded={playNext}
             width='100%'
             height='50vh'
-          />
+        />
           <div>
-            Playlist
-            
+          Playlist
+          <button onClick={handleClick}>Next</button>
           </div>
         </Layout>
   )
 }
-
+// for the yellow warning for dev tools: https://stackoverflow.com/questions/61339968/devtools-failed-to-load-sourcemap-could-not-load-content-for-chrome-extension
