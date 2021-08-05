2325d4233fbc501cef60a2887aaf2e2de4002b07
Elijah Cruz <elijah@Elijahs-MacBook-Pro.local>
2021-08-05 09:55:31 -0700

    fixed syntax errors:

--- client/src/services/playlists.js
+++ client/src/services/playlists.js
@@ -1,5 +1,4 @@
-// <<<<<<< test
-// import api from "./apiConfig";
+import api from "./apiConfig";
 
 
 export const createPlaylist = async (input) => {
@@ -31,30 +30,22 @@ export const getPlaylist = async (id) => {
 }
 
 
-// export const deletePlaylist = async (id) => {
-//   try {
-//     const res = await api.delete(`/playlist/${id}`);
-//     return res
-//   } catch (e) {
-//     throw e;
-//   }
-// };
-// export const updatePlaylist = async (id) => {
-//   try {
-//     const res = await api.put(`/playlist/${id}`);
-//     return res.data;
-//   } catch (e) {
-//     throw e;
-//   }
-// }
-// =======
-// import React from 'react'
+export const deletePlaylist = async (id) => {
+  try {
+    const res = await api.delete(`/playlist/${id}`);
+    return res
+  } catch (e) {
+    throw e;
+  }
+};
 
-// export default function playlists() {
-//   return (
-//     <div>
-      
-//     </div>
-//   )
-// }
-// >>>>>>> userSita
+
+export const updatePlaylist = async (id) => {
+  try {
+    const res = await api.put(`/playlist/${id}`);
+    return res.data;
+  } catch (e) {
+    throw e;
+  }
+}
+
