cede13e4acde4380ef00b542be2d860905823c90
Shahroze Husain <shahroze93@gmail.com>
2021-08-05 21:44:24 -0400

    got viewPage working

--- client/src/views/ViewPage/ViewPage.jsx
+++ client/src/views/ViewPage/ViewPage.jsx
@@ -8,6 +8,7 @@ import { useParams } from 'react-router';
 
 export default function ViewPage(props) {
   const [playlist, setPlaylist] = useState({})
+  const [loading, setLoading] = useState(false);
   const [links, setLinks] = useState([])
   const { id } = useParams()
   const data = [
@@ -17,37 +18,45 @@ export default function ViewPage(props) {
     "https://soundcloud.com/drokenji/drokenji-since-december-prod-census-cv?in=soundcloud-hustle/sets/the-lookout-tomorrows-rap-hits",
     "https://www.youtube.com/watch?v=Zj35vRgeXLs&t=1s",
   ]
+  const data2 = playlist.videoURLs
 
   useEffect(() => {
-  fetchPlaylist()
+    fetchPlaylist()
   }, [])
-  
+
+  useEffect(() => {
+    setCurrentVideo(newArray[0]); // This is be executed when `loading` state changes
+}, [loading])
+
+
   async function fetchPlaylist () {
     let res = await getPlaylist(id)
     // console.log(id)
     // console.log(res)
     if (res) {
       setPlaylist(res)
+      setLoading(true);
     }
-    setLinks(playlist.videoURLs)
   }
 
-  useEffect(() => {
-    setCurrentVideo(links)
-  }, [])
-  
+  let newArray = []
+  const allURLs = playlist?.links?.map((link) => {
+    // return link.linkURL
+    newArray.push(link.linkURL)
+  }) 
+
   const [trackIndex, setTrackIndex] = useState(0);
   const [currentVideo, setCurrentVideo] = useState([])
   const toPrevTrack = () => {
     if (trackIndex - 1 < 0) {
-      setTrackIndex(playlist.length - 1);
+      setTrackIndex(newArray.length - 1);
     } else {
-      setTrackIndex(trackIndex - 1);
+      setTrackIndex(0);
     }
     fetchVideo()
   }
   const toNextTrack = () => {
-    if (trackIndex < playlist.length - 1) {
+    if (trackIndex < newArray.length - 1) {
       setTrackIndex(trackIndex + 1);
     } else {
       setTrackIndex(0);
@@ -55,7 +64,9 @@ export default function ViewPage(props) {
     fetchVideo()
   }
   const fetchVideo = () => {
-    setCurrentVideo(playlist[trackIndex])
+      setCurrentVideo(newArray[trackIndex])
+      console.log(newArray[trackIndex])
+      console.log(trackIndex)
   }
   const handleWatchComplete = ({ played }) => {
     // console.log(played)
@@ -64,7 +75,6 @@ export default function ViewPage(props) {
       toNextTrack()
     } 
   }
-
 
 
     return (
@@ -76,7 +86,7 @@ export default function ViewPage(props) {
             <ReactPlayer
               className='react-player'
               controls={true}
-              url={currentVideo}
+              playing url={currentVideo}
               onProgress={handleWatchComplete}
               width='100%'
               height='50vh'
