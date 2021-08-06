31434b7b00b9175f3a0ca8e8690a75428871df43
Elijah Cruz <elijah@Elijahs-MacBook-Pro.local>
2021-08-05 18:49:26 -0700

    have to fix functionality

--- client/src/views/ViewPage/ViewPage.jsx
+++ client/src/views/ViewPage/ViewPage.jsx
@@ -18,9 +18,13 @@ export default function ViewPage(props) {
   // ]
 
   useEffect(() => {
-  fetchPlaylist()
+    fetchPlaylist()
+    setCurrentVideo(allURLs)
   }, [])
   
+  const allURLs = playlist?.links?.map((link) => {
+    return link.linkURL
+  })
   const fetchPlaylist = async () => {
     const res = await getPlaylist(id)
     console.log(id)
@@ -29,68 +33,70 @@ export default function ViewPage(props) {
       setPlaylist(res)
     }
   }
+  // const data = [playlist?.links[0]?.linkURL]
+  console.log("THIESE IS LINKS", playlist)
 
-  // const playlistItems = playlist.map((link, index) =>
-  // <div key={index}> 
-  //     <h3>{index}. {link}</h3>
-  // </div>
+  // const playlistItems = playlist?.links?.map((link, index) =>
+  //   <div key={index}>
+  //     <h3>{index}. {link.linkURL}</h3>
+  //   </div>
   // );
-  // const [currentVideo, setCurrentVideo] = useState(playlist[0])
-  // const [trackIndex, setTrackIndex] = useState(0);
-  // const toPrevTrack = () => {
-  //   if (trackIndex - 1 < 0) {
-  //     setTrackIndex(playlist.length - 1);
-  //   } else {
-  //     setTrackIndex(trackIndex - 1);
-  //   }
-  //   fetchVideo()
-  // }
-  // const toNextTrack = () => {
-  //   if (trackIndex < playlist.length - 1) {
-  //     setTrackIndex(trackIndex + 1);
-  //   } else {
-  //     setTrackIndex(0);
-  //   }
-  //   fetchVideo()
-  // }
-  // const fetchVideo = () => {
-  //   setCurrentVideo(playlist[trackIndex])
-  // }
-  // const handleWatchComplete = ({ played }) => {
-  //   // console.log(played)
-  //   if (played >= 0.99) {
-  //     console.log("Done!")
-  //     toNextTrack()
-  //   } 
-  // }
+  
+  const [currentVideo, setCurrentVideo] = useState([])
+  console.log("THIS IS CURRENT VIDEO",currentVideo)
+  const [trackIndex, setTrackIndex] = useState(0);
+  const toPrevTrack = () => {
+    if (trackIndex - 1 < 0) {
+      setTrackIndex(playlist.length - 1);
+    } else {
+      setTrackIndex(trackIndex - 1);
+    }
+    fetchVideo()
+  }
+  const toNextTrack = () => {
+    if (trackIndex < playlist.length - 1) {
+      setTrackIndex(trackIndex + 1);
+    } else {
+      setTrackIndex(0);
+    }
+    fetchVideo()
+  }
+  const fetchVideo = () => {
+    setCurrentVideo(playlist[trackIndex])
+  }
+  const handleWatchComplete = ({ played }) => {
+    // console.log(played)
+    if (played >= 0.99) {
+      console.log("Done!")
+      toNextTrack()
+    }
+  }
     return (
-        <Layout user={props.user} setUser={props.setUser}>
-          {/* <h1>PLAYLIST NAME</h1>
-        <h3>SHAHROZE</h3>
-            <ReactPlayer
-              className='react-player'
-              controls={true}
-              playing url={currentVideo}
-              onProgress={handleWatchComplete}
-              width='100%'
-              height='50vh'
+      <Layout user={props.user} setUser={props.setUser}>
+        <h1>PLAYLIST NAME</h1>
+        <h3>{playlist?.title}</h3>
+        <ReactPlayer
+          className='react-player'
+          controls={true}
+          playing url={allURLs}
+          onProgress={handleWatchComplete}
+          width='100%'
+          height='50vh'
         />
         <button onClick={toPrevTrack}>PREV</button>
         <button onClick={toNextTrack}>NEXT</button>
-          <div>
+        <div>
           <h3>Playlist Items</h3>
-          <div>{playlistItems}</div>
-          <NewComment user={props.user} setUser={props.setUser}/>
-          </div> */}
-        {playlist?.title}
         {playlist?.links?.map((link) => {
           return (
             <p>{link.linkURL}</p>
           )
           
         })}
-        </Layout>
-  )
-}
+          <NewComment user={props.user} setUser={props.setUser} />
+        </div>
+      </Layout>
+    )
+  }
 // for the yellow warning for dev tools: https://stackoverflow.com/questions/61339968/devtools-failed-to-load-sourcemap-could-not-load-content-for-chrome-extension
-// REACTPLAYER can take in an array of youtube videos but cannot take in an array of soundcloud songs
+// // REACTPLAYER can take in an array of youtube videos but cannot take in an array of soundcloud songs
--- controllers/playlists.js
+++ controllers/playlists.js
@@ -18,7 +18,7 @@ export const getAllPlaylists = async (req, res) => {
 export const getPlaylist = async (req, res) => {
   try {
     let { id } = req.params;
-    const playlist = await Playlist.findById(id).populate("links");
+    const playlist = await Playlist.findById(id).populate("links").populate("userId");
     if (playlist) {
       res.send(playlist);
     }
