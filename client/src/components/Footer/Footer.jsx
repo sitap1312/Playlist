f848eb61c2599f3895ac42bbc464f29a925a42e4
Shahroze Husain <shahroze93@gmail.com>
2021-08-09 09:28:30 -0400

    Added Footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import "./Footer.css"
+import React from "react";
+import "./Footer.css"
 
-export default function Footer() {
+function Footer (props) {
   return (
-    <div>
-      Footer
-    </div>
+    <footer className="footer">
+      <p>© Playlistify 2021</p>
+      <div><a className="fab fa-linkedin" href="https://www.linkedin.com/in/shahrozehusain" ></a>S Husain</div>
+      <div><a className="fab fa-linkedin" href="https://www.linkedin.com/in/elijah-cruz" ></a>E Cruz</div>
+      <div><a className="fab fa-linkedin" href="https://www.linkedin.com/in/Dhiggins94" ></a>D Higgins</div>
+      <div><a className="fab fa-linkedin" href="https://www.linkedin.com/in/sita-pataballa-41963b206" ></a>S Pataballa</div>
+      <div><a className="fab fa-linkedin" href="https://www.linkedin.com/in/sherget15" ></a>S Herget</div>
+    </footer>
   )
 }
+
+export default Footer
