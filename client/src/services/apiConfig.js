a256efc46a8b653287f3fb7e13558551640181da
Elijah Cruz <elijah@Elijahs-MacBook-Pro.local>
2021-08-09 15:32:55 -0700

    finished deployment

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -9,7 +9,7 @@ const getToken = () => {
 const api = axios.create({
   baseURL:
     process.env.NODE_ENV === 'production'
-      ? "https://infinite-cove-01476.herokuapp.com/"
+      ? "https://playlistify4real.herokuapp.com/"
       : "http://localhost:4567/api",
 });
 
