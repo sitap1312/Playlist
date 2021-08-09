eea4b2bea19e619621df9ef63add417762e4bed2
Shahroze Husain <shahroze93@gmail.com>
2021-08-08 23:15:23 -0400

    Navbar working

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,3 +1,4 @@
+import "./Navbar.css"
 import React from "react";
 import { Link } from "react-router-dom";
 import { signOut } from "../../services/users";
@@ -13,22 +14,43 @@ export default function Nav(props) {
   };
 
   return (
-    <div>
-      <Link to="/">PLAYLISTIFY</Link>
-      {props.user ? (
-        <>
-          <Link to="/my-account">Hi!, {props.user?.username}</Link>
-          <Link to="/create-playlist">CREATE PLAYLIST</Link>
-          <button onClick={handleSignOut}>Sign Out</button>
-          <Link to="/edit-user">Edit account</Link>
-
-        </>
-      ) : (
-        <div>
-          <Link to="/sign-up">Sign Up</Link>
-          <Link to="/sign-in">Sign In</Link>
+    <section className= "navbar-page"> 
+        <div className="navbar-logo">   
+        <Link to="/" className="navLogoContainer">
+          <img className="nav-logo" src="https://i.imgur.com/xidyTP5.png" alt="logo"/>
+          <div lassName="title" >PLAYLISTIFY</div>
+              
+        </Link>
         </div>
-      )}
-    </div>
+            <div className="options"> 
+      {props.user ? (
+          <>
+            <div className="navbar-options">
+              <Link className="nav-link" to="/my-account">Hi! {props.user?.username} </Link>
+            </div>
+
+            <div className="navbar-options">
+              <Link   className="nav-link" to="/create-playlist">Create Playlist</Link>
+            </div>
+
+            <div className="navbar-options">
+            <Link   className="nav-link" to="/edit-user"> Edit Account </Link>
+          </div>
+          <div className="navbar-options" >
+            <button className="signout-btn" type="submit" onClick={handleSignOut}>SIGN OUT</button>
+              </div>
+        </>
+        ) : (
+        <div className="loginButtonContainer">
+        <div className="navbar-options">
+          <Link  className="loginButtons" to="/sign-up">SIGN UP</Link>
+          </div>
+          <div className="navbar-options">
+          <Link className="loginButtons" to="/sign-in">SIGN IN</Link>
+          </div>
+          </div>
+        )}
+        </div>
+      </section>
   );
 }
